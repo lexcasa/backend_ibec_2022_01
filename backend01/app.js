efe3d190d07c2f0a26c56544a5569a2e6f197910
Alex Casadevall <alex@lexartlabs.com>
2022-03-11 20:43:11 -0300

    add: services and new notes

--- backend01/app.js
+++ backend01/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {suma, resta} = require('./services/calculadora.service')
+// const Calculadora = require('./services/calculadora.service')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const numTotal = suma(1,2)
+
+  res.send({respuesta: 'Hello World! suma: ' + numTotal + ' resta: ' + resta(1,1)})
 })
 
 app.listen(port, () => {
---
+++ backend01/services/calculadora.service.js
@@ -0,0 +1,18 @@
+const Calculadora = {
+    suma: function (a, b){
+        return a + b
+    },
+    resta: function (a, b){
+        return a - b
+    },
+    multi: function (a, b){
+        return a * b
+    },
+    divi: function (a, b){
+        if(b !== 0){
+            return a / b
+        }
+        return 'error'
+    }
+}
+module.exports = Calculadora;
